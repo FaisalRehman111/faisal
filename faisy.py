9d43fae3c26e0f9da1f0a8f8c0ba65342227b2a1
FaisalRehman111 <94007492+FaisalRehman111@users.noreply.github.com>
2022-09-29 15:10:43 +0500

    Delete faisy.py

--- faisy.py
+++
@@ -1,9 +0,0 @@
-import os, platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from FLASTffs import super_fsl
-    super_fsl()
-elif bit == '32bit':
-    exit('\x1b[1;91m\n\tOpps not upload')
-
