8aa14dd05a80f49c20790a1cfb36e887e1348d22
FaisalRehman111 <94007492+FaisalRehman111@users.noreply.github.com>
2024-06-07 16:23:16 +0500

    Update faisy.py

--- faisy.py
+++ faisy.py
@@ -1,27 +1,35 @@
-import os, platform, time, pycurl
-os.system('clear')
-print('checking updates')
-os.system('git pull')
-print('first allow permations (y)')
-os.system('termux-setup-storage')
+import os
+import platform
+import subprocess
+import time
+
+subprocess.call('clear', shell=True)
+
+print('Checking for updates...')
+
+subprocess.call('git pull', shell=True)
+
+
+permission = input('Do you want to allow permissions? (y/n): ')
+if permission.lower() == 'y':
+    subprocess.call('termux-setup-storage', shell=True)
+
 try:
     import requests
-except:
-    os.system('pip install requests')
-try:
-    import pycurl
-except:
-    os.system('pip install pycurl')
-os.system('git pull')
-import requests
+except ImportError:
+    subprocess.call('pip install requests', shell=True)
+
+subprocess.call('git pull', shell=True)
+
+
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
+    print("\nCongratulations! Your device supports this tool.")
     from FSL import menu___
     menu___()
 elif bit == '32bit':
+    print("\nCongratulations! Your device supports this tool.")
     from FSL32 import menu___
-    print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
-
-
-
+    menu___()
+else:
+    print("Jaa Bhai Maaf Kr:", bit)
